085678932629ffb768c96fe0e7f93ec22290b256
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-08 23:57:20 -0300

    Adicionada as interfaces do carrinho

---
+++ GeekShopping.Web/Models/CartViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace GeekShopping.Web.Models {
+    public class CartViewModel {
+        public  CartHeaderViewModel CartHeaderViewModel { get; set; }
+        public IEnumerable<CartDetailViewModel> CartDetails { get; set; }
+    }
+}
